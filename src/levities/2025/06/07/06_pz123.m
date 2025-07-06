3a01cbf3d9ec1b68674e282e1a8d1808727a9621
Ralf Funke <liedzeit@icloud.com>
2025-07-06 16:14:27 +0200

    pz123

---
+++ src/levities/2025/06/07/06_pz123.m
@@ -0,0 +1,21 @@
+---
+title: "Dept. Bilderrätsel – 123"
+date: "2025-07-06"
+tags: ["levity","picpuzzle"]
+keywords: ["Popeye","Douglas Fairbanks","Nicolas Cage"]
+---
+Fünf Lösungen. Sehr schön, es darf wieder ein wenig anspruchsvoller werden. Was haben die folgenden Bilder gemeinsam?
+ <br/>
+
+<img  src="/assets/img/picpuzzle/picpuzzle123.webp" alt="Bilderrätsel123">
+
+<br/>
+<br/>
+<br/>
+
+Tip: Vielleich müsst ihr diesmal eure Eltern oder ältere Nachbarn um Hilfe bitten.
+
+<br/>
+<br/>
+
+<sup>Auflösung pz122: Zu sehen waren Douglas Fairbanks als Sindbad the <i>Sailor</i>, a Glass of Champagne, der zweite Hit von <i>Sailor</i>, Popeye, der natürlich auch ein <i>Sailor</i> ist und Nicolas Cage als <i>Sailor</i> in Wild at Heart. <sup>
